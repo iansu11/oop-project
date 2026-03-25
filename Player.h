25cde3c271257b4bdd8ca0584f7fe6347bc6c830
ian su <twiansu@gmail.com>
2026-03-25 19:13:12 +0800

    player .cpp .h 初次修改

--- Player.h
+++ Player.h
@@ -1,11 +1,29 @@
 #ifndef PLAYER_H
 #define PLAYER_H
+#include <string>
+#include <vector>
+using std::string;
+using std::vector;
 
 class Player{
 private:
-
+	string name;
+	int money,position;
+	bool prison,bankruptcy;
+	vector<int> ownedLands;
 public:
+	Player(string n0, int m0);
+	string getName();
+	int getMoney();
+	int getPosition();
+	bool isprison();
+	bool isbankruptcy();
 
+	int addMoney(int amount);
+	int payMoney(int amount);
+
+	void buyLand(int landid);
+	void sellLand(int landid,int price);
 
 };
 
